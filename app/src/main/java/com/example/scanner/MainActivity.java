c6a3be1372f7deb29a89e404716b9e6bd952e6e5
sushantmakadia <makadiasushant@gmail.com>
2019-07-04 16:21:09 +0530

    first

---
+++ app/src/main/java/com/example/scanner/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.scanner;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class MainActivity extends AppCompatActivity {
+Button b1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b1=findViewById(R.id.b1);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator integrator = new IntentIntegrator(MainActivity.this);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+                integrator.setPrompt("scan");
+                integrator.setCameraId(0);
+                integrator.setBeepEnabled(false);
+              //  integrator.setBarcodeImageEnabled(false);
+                integrator.initiateScan();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                Toast.makeText(this, "failed", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, result.getContents(), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
